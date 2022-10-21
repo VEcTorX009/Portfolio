ecd8c46e1b31defa9c62fa108f26fd060ecb0894
Som <85481905+VEcTorX009@users.noreply.github.com>
2022-10-21 19:15:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function letsgo() {
       scaleY: [0, 1],
       opacity: 0,
       easing: "easeOutExpo",
-      duration: 300,
+      duration: 400,
     })
     .add({
       targets: ".ml11 .line",
